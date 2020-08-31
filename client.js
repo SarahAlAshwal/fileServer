7d4cdf5c6818d325c4835b3f8e71171a6a43e6d8
SarahAlAshwal <sa.alashwal@gmail.com>
2020-08-31 03:03:05 +0000

    implementing sending files

--- client.js
+++ client.js
@@ -1,20 +1,46 @@
 const net = require('net');
+const fs = require('fs');
+remote_server = process.argv[2];
+let socket;
 
-const conn = net.createConnection({ 
-  // change to IP address of computer or ngrok host if tunneling
- port: 3000,
- host: 'localhost' // or change to the ngrok port if tunneling
+socket = remote_server ? net.connect(3000, remote_server): net.connect(3000);
+let ostream = fs.createWriteStream("./testReceive.txt");
+let date = new Date(), size = 0, elapsed; 
+socket.on('data', chunk => {
+  size += chunk.length;
+  elapsed = new Date() - date;
+  socket.write(`\r${(size/ (1024 * 1024)).toFixed(2)} MB of data was sent. Total time elapsed is ${elapsed/1000} s`);
+  ostream.write(chunk);
 });
 
-conn.setEncoding('utf8');
-
-conn.on('connect', () => {
-  conn.write("test.txt");
+socket.on('end', () => {
+  console.log(`\nFinished getting file. speed was ${((size / (1024 * 1024)) / (elapsed / 1000)).toFixed(2)} MB/s`);
+  process.exit();
 });
 
-conn.on('data', (data) => {
-  console.log('Server says: ', data);
-});
+
+
+
+
+
+
+// const net = require('net');
+
+// const conn = net.createConnection({ 
+//   // change to IP address of computer or ngrok host if tunneling
+//  port: 3000,
+//  host: 'localhost' // or change to the ngrok port if tunneling
+// });
+
+// conn.setEncoding('utf8');
+
+// conn.on('connect', () => {
+//   conn.write("test.txt");
+// });
+
+// conn.on('data', (data) => {
+//   console.log('Server says: ', data);
+// });
 
 
 
--- fileServer.js
+++ fileServer.js
@@ -1,13 +1,46 @@
 const net = require ('net');
-const server = net.createServer();
-server.on('connection', (client) => {
-  client.write('Which file do you need? ');
-  client.setEncoding('utf8');
-  client.on('data', (data) => {
-    console.log("The required file: ", data);
+const fs = require ('fs');
+let server, istream = fs.createReadStream ("./test.txt");
+server = net.createServer( socket => {
+  socket.pipe(process.stdout);
+  istream.on("readable", function () {
+    let data;
+    while (data = this.read()) {
+      socket.write(data);
+    }
+  });
+  istream.on("end", function() {
+    socket.end();
+  });
+  istream.on('end', function() {
+    server.close(() => {
+      console.log("\nTransfer is done!");
+    }); 
   });
 });
 
 server.listen(3000, () => {
-  console.log("Server listening on port 3000! ");
+  console.log('Server listening on port 3000');
 })
+
+
+
+// const fs = require('fs');
+// const net = require ('net');
+// const server = net.createServer();
+// server.on('connection', (client) => {
+//   client.write('Which file do you need? ');
+//   client.setEncoding('utf8');
+//   client.on('data', (data) => {
+//     console.log("Reading file ");
+//     fs.readFile(`./${data}`, 'utf8', (error,fileText) => {
+//       client.write(fileText);
+//     } )
+//   });
+
+  
+// });
+
+// server.listen(3000, () => {
+//   console.log("Server listening on port 3000! ");
+// })
